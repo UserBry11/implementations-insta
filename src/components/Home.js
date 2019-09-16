541c848c68341ae54f257f32c83da536e640d30d
Bryan Fernandez <bryfern710@gmail.com>
2019-09-16 15:14:36 -0500

    asdf

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { getImagesResponse, postImageResponse } from "../apiStubs";
+//import { getImagesResponse, postImageResponse } from "../apiStubs";
+import { getImages, postImage } from "../clientApi";
 
 class Home extends React.Component {
   state = { 
@@ -8,14 +9,18 @@ class Home extends React.Component {
   };
 
   componentDidMount(){
-    this.setState( {...this.state, getImages: getImagesResponse });
+    getImages().then(result => {
+      this.setState( {...this.state, getImages: result });
+    })
   }
 
   handleUploadImage = event => {
     event.preventDefault();
-    // const formData = new FormData(event.target)
-    this.setState({ ...this.state, postImage: postImageResponse});
-  }
+    const formData = new FormData(event.target)
+    postImage(formData).then(result => {
+      this.setState({ ...this.state, postImage: result});
+    });
+  };
 
   render() {
     return (
@@ -24,7 +29,7 @@ class Home extends React.Component {
           <input type="file" />
           <button type="submit">Upload Image, Please</button>
         </form>
-
+        {this.state.postImage.statusCode === 200 && "Upload was succesful"}
         <h1>Kenziegram</h1>;
         {this.state.getImages.imageURIs.map(uri => (
           <img key={uri} src={uri} />
